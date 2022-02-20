a1b12411c49e4dbbefb392869fdff5f22d140346
santosh singh <santosh_singh2@dell.com>
2022-02-20 19:07:21 +0800

    Fixed connec

--- Startup.cs
+++ Startup.cs
@@ -65,16 +65,15 @@ namespace code_maze_cosmosdb
         /// <returns></returns>
         private static async Task<CosmosDbService> InitializeCosmosClientInstanceAsync(IConfigurationSection configurationSection)
         {
-            var databaseName = configurationSection["DatabaseName"];
-            var containerName = configurationSection["ContainerName"];
-            var account = configurationSection["Account"];
-            var key = configurationSection["Key"];
-
-            var client = new Microsoft.Azure.Cosmos.CosmosClient(account, key);
-            var database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+            string databaseName = configurationSection.GetSection("DatabaseName").Value;
+            string containerName = configurationSection.GetSection("ContainerName").Value;
+            string account = configurationSection.GetSection("Account").Value;
+            string key = configurationSection.GetSection("Key").Value;
+            Microsoft.Azure.Cosmos.CosmosClient client = new Microsoft.Azure.Cosmos.CosmosClient(account, key);
+            CosmosDbService cosmosDbService = new CosmosDbService(client, databaseName, containerName);
+            Microsoft.Azure.Cosmos.DatabaseResponse database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
             await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
 
-            var cosmosDbService = new CosmosDbService(client, databaseName, containerName);
             return cosmosDbService;
         }
     }
